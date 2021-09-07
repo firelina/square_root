b6bba5c45977bcd6b22f7e8576b1ffb1017e9561
AlexanderHSE <90000515+AlexanderHSE@users.noreply.github.com>
2021-09-07 19:50:01 +0500

    фигня с клавой(подробнее смотреть на мэине2)

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import cmath
 import math
 
 
-
-
 # Validating input values and calculating result value
 def calculate(num, prec):
-    dictt = {'input' : ['Incorrect input', 'Entrada incorrecta', '输入不正确', 'Неверный ввод'],
-             'ent_num' : ['Please, enter a number', 'Por favor, introduzca un número', '请输入数字', 'Пожалуйста, введите число']}
+    dictt = {'input': ['Incorrect input', 'Entrada incorrecta', '输入不正确', 'Неверный ввод'],
+             'ent_num': ['Please, enter a number', 'Por favor, introduzca un número', '请输入数字',
+                         'Пожалуйста, введите число']}
 
     if (state_.get() == True):
         try:
@@ -120,11 +119,13 @@ class CreateToolTip(object):
 
 
 keys_lang = []
-keys_trans = ["title", "text", 'del_num', 'del_per', 'lang', 'help', 'aboutus', 'enter_num_tip', 'enter_per_tip', 'input', 'ent_num']
+keys_trans = ["title", "text", 'del_num', 'del_per', 'lang', 'help', 'aboutus', 'enter_num_tip', 'enter_per_tip',
+              'input', 'ent_num']
 
 # trans = {'English': {"title": 'Square root of a number', "text": 'Calculate', 'del_num': 'Remove number',
 #           'del_per': 'Remove precision', 'lang': 'Language', 'help': 'Help', 'aboutus': 'About us'}}
 trans = {}
+
 
 # TRANSLATE
 def lang_change(*args):
@@ -142,28 +143,76 @@ def lang_change(*args):
                                 trans[lang_now.get()]["enter_per_tip"])
 
 
-
-# Check state
-def check_state():
-    if (state_.get() == True):
-        set_prec.config(state='normal')
-    else:
-        set_prec.config(state='readonly')
-
-
 # CLEAR
 def clear():
+    txt['state'] = 'normal'
     txt.delete(0, 'end')
+    txt['state'] = 'readonly'
 
 
 def clearper():
-    set_prec.delete(0,'end')
+    set_prec['state'] = 'normal'
+    set_prec.delete(0, 'end')
+    set_prec['state'] = 'readonly'
 
 
+# start_calc
 def clicked():
     num = txt.get()
     prec = set_prec.get()
     ans.set(calculate(num, prec))
+
+
+#   set_symbol_functions
+def set_number_symbol(value):
+    txt['state'] = 'normal'
+    txt.delete(0, 'end')
+    txt.insert(0, value)
+    txt['state'] = 'readonly'
+
+
+def set_prec_symbol(value):
+    set_prec['state'] = 'normal'
+    set_prec.delete(0, 'end')
+    set_prec.insert(0, value)
+    set_prec['state'] = 'readonly'
+
+
+# key_events
+
+def press_key(event):
+    if not state_.get():
+        if event.char == '\r':
+            clicked()
+        elif event.char == '\x08':
+            value = txt.get()
+            value = value[:-1]
+            set_number_symbol(value)
+        elif event.char.isdigit() or event.char in '+-..j':
+            value = txt.get() + event.char
+            if event.char.isdigit():
+                set_number_symbol(value)
+            elif len(value) > 1 and value[-1] == value[-2]:
+                value = value[:-1]
+            elif len(value) > 1 and value[-2] == '-' and value[-1] == '+':
+                value = value[:-2] + '+'
+            elif len(value) > 1 and value[-2] == '+' and value[-1] == '-':
+                value = value[:-2] + '-'
+            set_number_symbol(value)
+    else:
+        if event.char == '\x08':
+            value = set_prec.get()
+            value = value[:-1]
+            set_prec_symbol(value)
+        elif event.char == '\r':
+            clicked()
+        elif event.char.isdigit():
+            set_prec_symbol(set_prec.get() + event.char)
+        elif event.char in '..':
+            value=set_prec.get() + event.char
+            if len(value) > 1 and value[-1] == value[-2]:
+                value=value[:-1]
+            set_prec_symbol(value)
 
 
 # Creating window
@@ -172,7 +221,8 @@ window.geometry('500x130')
 window.resizable(width=False, height=False)
 window.title('Square root of a number')
 photo = PhotoImage(file='logo.png')
-window.iconphoto(False,photo)
+window.iconphoto(False, photo)
+window.bind('<Key>', press_key)
 '''
 Creating elements
 '''
@@ -181,13 +231,11 @@ lang_now = StringVar()
 lang_now.set('English')
 lang_now.trace("w", lang_change)
 
-
 ans = StringVar()
 ans.set('')
 
 # MENU
 main_menu = Menu()
-
 
 
 # files = ['eng', 'spn', 'rus', 'cha']
@@ -203,7 +251,6 @@ def add_butts():
         empty_menu.add_cascade(label='Language', menu=lang_m)
         empty_menu.add_cascade(label='Help', menu=about_m)
 
-
     f = open("all_languages", encoding="utf-8")
     data = f.read()
     data = data.split('***\n')
@@ -218,10 +265,8 @@ def add_butts():
             trans[lang_title][keys_trans[i]] = phrases[i]
         lang_m.add_radiobutton(label=lang_title, variable=lang_now, value=lang_title)
     f.close()
-    # print(trans)
+    #print(trans)
     window.after(5000, add_butts)
-
-
 
 
 # lang_m.add_radiobutton(label="English", variable=lang_now, value="English")
@@ -237,13 +282,12 @@ add_butts()
 about_m = Menu()
 about_m.add_command(label="About us")
 
-
 main_menu.add_cascade(label='Help', menu=about_m)
 # OUTPUT area
 out = Entry(window, textvariable=ans, width=83, justify='right', relief='sunken', state='readonly')
 out.place(x=0, y=0)
 # INPUT area
-txt = Entry(window, width=30,justify='right')
+txt = Entry(window, width=30, justify='right', state='readonly')
 txt.place(x=20, y=60)
 INPT_TIP = CreateToolTip(txt, \
                          'Enter in this field the number whose square root you want to calculate. '
@@ -253,10 +297,10 @@ INPT_TIP = CreateToolTip(txt, \
 # Calculate with given PRECISION
 state_ = BooleanVar()
 state_.set(0)
-chk = Checkbutton(window, variable=state_, onvalue=1, offvalue=0,command=check_state)
+chk = Checkbutton(window, variable=state_, onvalue=1, offvalue=0)
 chk.place(x=300, y=60)
 ##Entry for PREC
-set_prec = Entry(window, width=22, relief='sunken', state='readonly',justify='right')
+set_prec = Entry(window, width=22, relief='sunken', state='readonly', justify='right')
 set_prec.place(x=320, y=60)
 ###TIP for USER
 loadimage = PhotoImage(file='yes4.png')
@@ -276,9 +320,8 @@ btn_del_all.place(x=95, y=83)
 btn_del_all_per = Button(window, text='Remove precision', command=clearper)
 btn_del_all_per.place(x=320, y=83)
 
-
 # Creating BUTTON
-btn = Button(window, text='Calculate',  command=clicked)
+btn = Button(window, text='Calculate', command=clicked)
 btn.place(x=20, y=83)
 
 window.config(menu=main_menu)
